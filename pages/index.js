2cc57df46e222259a4eed6779b9d576f2c37ddc2
andreamiele <andrea.miele.pro@gmail.com>
2022-01-25 17:57:47 +0100

    test4

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ const Home = () => (
             maxWidth="100px"
             display="inline-block"
             borderRadius="full"
-            src="/images/takuya.jpg"
+            src="/images/takuya.jpeg"
             alt="Profile image"
           />
         </Box>
