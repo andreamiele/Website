8189edc3353d3ed18e3d16a40f4f1b542625354a
andreamiele <andrea.miele.pro@gmail.com>
2022-01-27 18:14:24 +0100

    test5

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -97,24 +97,9 @@ const VoxelDog = () => {
       light.shadow.camera.near = 0.5; // default
       light.shadow.camera.far = 500; // default
 
-//Create a sphere that cast shadows (but does not receive them)
-      const sphereGeometry = new THREE.SphereGeometry( 5, 32, 32 );
-      const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
-      const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-      sphere.castShadow = true; //default is false
-      sphere.receiveShadow = false; //default
-      scene.add( sphere );
 
-//Create a plane that receives shadows (but does not cast them)
-      const planeGeometry = new THREE.PlaneGeometry( 20, 20, 32, 32 );
-      const planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
-      const plane = new THREE.Mesh( planeGeometry, planeMaterial );
-      plane.receiveShadow = true;
-      scene.add( plane );
 
-//Create a helper for the shadow camera (optional)
-      const helper = new THREE.CameraHelper( light.shadow.camera );
-      scene.add( helper );
+
       let req = null
       let frame = 0
       const animate = () => {
